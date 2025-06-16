18e26d43930dcd1428a57bf8ff3fe9e0e78ee1d8
Gabriel Melo <araujosemacento@alu.ufc.br>
2025-06-16 15:04:42 -0300

    maldito cors

--- rxconfig.py
+++ rxconfig.py
@@ -4,5 +4,5 @@ config = rx.Config(
     app_name="ed_acima_das_nuvens",
     tailwind={"plugins": ["@tailwindcss/forms", "@tailwindcss/typography"]},
     plugins=[rx.plugins.TailwindV3Plugin()],
-    cors_allowed_origins=["http://localhost:3000", "https://*.github.io/*",],
+    cors_allowed_origins=["http://localhost:3000", "https://araujosemacento.github.io/ED-Acima_das_Nuvens/",],
 )
