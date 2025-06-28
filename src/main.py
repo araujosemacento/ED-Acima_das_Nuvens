8e2b8f6d1ab9f8ec1bede3871a1f2fb406428ead
Gabriel Melo <araujosemacento@alu.ufc.br>
2025-06-27 23:59:46 -0300

    bulma

--- src/main.py
+++ src/main.py
@@ -12,11 +12,11 @@ if window.localStorage.getItem("theme") is None:
     dom.documentElement.setAttribute("data-theme", window.localStorage.getItem("theme"))
 
 
-""" def theme_toggle(event):
-    current_theme = dom.documentElement.getAttribute("data-theme")
-    new_theme = "dark" if current_theme == "light" else "light"
-    dom.documentElement.setAttribute("data-theme", new_theme)
-    window.localStorage.setItem("theme", new_theme) """
+# def theme_toggle(event):
+#    current_theme = dom.documentElement.getAttribute("data-theme")
+#    new_theme = "dark" if current_theme == "light" else "light"
+#    dom.documentElement.setAttribute("data-theme", new_theme)
+#    window.localStorage.setItem("theme", new_theme)
 
 
-dom.getElementById("sampleButton").addEventListener("click", lambda e: theme_toggle(e))
+# dom.getElementById("sampleButton").addEventListener("click", lambda e: theme_toggle(e))
